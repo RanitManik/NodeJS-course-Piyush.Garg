cd5c4eaa8cf2ef8400abdee47b9729bc1ce04da5
Ranit Manik <138437760+RanitManik@users.noreply.github.com>
2024-04-22 12:34:19 +0530

    added files

---
+++ 21. Building Node.js Authentication/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const path = require('path');
+const {connectMongoDB} = require('./connect')
+
+const urlRoute = require('./routes/url');
+const staticRoute = require('./routes/staticRouter');
+const userRoute = require('./routes/user');
+
+const app = express();
+const PORT = 5000;
+const url = "mongodb://127.0.0.1:27017/url-shortener";
+
+connectMongoDB(url, (err, db) => {
+    if (err) {
+        console.error(err);
+    }
+}).then(() => {
+    console.log("Connected to MongoDB");
+});
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+
+app.use("/", urlRoute);
+app.use("/", staticRoute);
+app.use("/", userRoute);
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
